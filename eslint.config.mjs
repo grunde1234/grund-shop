c82fe1536d19a422f300b143a842abc4ba0fe6a9
Okafor Ebuka <oebuka198@gmail.com>
2025-10-02 00:06:38 +0100

    prisma ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,12 +12,21 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    "rules": {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-require-imports": "warn",
+      "@typescript-eslint/no-this-alias": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+    }
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "src/generated/**",
     ],
   },
 ];
