f735c2b1ea88ce60c96721cf05ddc9b7b82333a7
ebuka <oebuka198@gmail.com>
2026-05-03 18:15:01 +0100

    tooltip

--- src/app/admin/overview/charts.tsx
+++ src/app/admin/overview/charts.tsx
@@ -1,15 +1,53 @@
-"use client"
-import {BarChart, Bar, XAxis, YAxis, ResponsiveContainer} from 'recharts'
-const Charts = ({data:{salesData}}:{data:{salesData:{month: string; totalSales: number}[]}}) => {
+"use client";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+
+const Charts = ({
+  data: { salesData },
+}: {
+  data: { salesData: { month: string; totalSales: number }[] };
+}) => {
   return (
     <ResponsiveContainer width="100%" height={300}>
-      <BarChart data={salesData}>
-        <XAxis dataKey="month" stroke='#888888' fontSize={12} tickLine={false} axisLine={false} />
-        <YAxis stroke='#888888' fontSize={12} tickLine={false} axisLine={false} tickFormatter={(value)=> `$${value}`}/>
-        <Bar dataKey="totalSales" fill="currentColor" radius={[4,4,0,0]} className='fill-primary'/>
+      <BarChart data={salesData}>{/* [{month:..., totalSale:...},....] */}
+        <XAxis
+          dataKey="month"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />{/* this takes the month */}
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `$${value}`}
+        />{/* Handles the range for the sales */}
+        <Tooltip
+          formatter={(value) => `$${value}`}
+          contentStyle={{
+            backgroundColor: "#1f2937", // dark background
+            border: "none",
+            borderRadius: "8px",
+            color: "#fff",
+          }}
+        />
+        <Bar
+          dataKey="totalSales"
+          fill="currentColor"
+          radius={[4, 4, 0, 0]}
+          className="fill-primary"
+        />
       </BarChart>
     </ResponsiveContainer>
-  )
-}
+  );
+};
 
-export default Charts
+export default Charts;
