75c12840404e6eb5474b47ad83928da89ee66335
Okafor Ebuka <oebuka198@gmail.com>
2026-09-25 13:10:23 +0100

    webhook to mark as paid

--- src/lib/actions/order.action.ts
+++ src/lib/actions/order.action.ts
@@ -188,7 +188,7 @@ export async function approvePaypalOrder(
 }
 
 // update the order to paid
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
