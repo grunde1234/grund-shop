93d8ef5a58e404bde0a3e1d19e3d7a65ebb48e5c
Okafor Ebuka <oebuka198@gmail.com>
2026-09-25 21:35:50 +0100

    toFixed(2)

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -17,7 +17,7 @@ export async function POST (req: NextRequest){
         //Update order status
         await updateOrderToPaid({
             orderId: object.metadata.orderId,
-            paymentResult: {id: object.id, status: 'COMPLETED', email_address: object.billing_details.email!, pricePaid: (object.amount/ 100).toFixed(), name: ''}
+            paymentResult: {id: object.id, status: 'COMPLETED', email_address: object.billing_details.email!, pricePaid: (object.amount/ 100).toFixed(2), name: ''}
         });
 
         return NextResponse.json({
